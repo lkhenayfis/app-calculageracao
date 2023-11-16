943354f88f7f327258b9c4449a43e8290a03a47d
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2023-11-16 11:20:03 -0300

    Add fun p leitura de polijus

--- R/leitura.r
+++ R/leitura.r
@@ -12,3 +12,16 @@ le_colina <- function(codigo, colinas, bucket, prefixo, localdir = "data") {
         return(colinas)
     }
 }
+
+le_polijus <- function(codigo, bucket, prefixo, localdir = "data") {
+    nomes <- paste0("polijus_", codigo, ".rds")
+    if (!missing(bucket)) {
+        nomes   <- file.path(prefixo, nomes)
+        colinas <- lapply(nomes, function(nome) aws.s3::s3read_using(readRDS, object = nome, bucket = bucket))
+        return(colinas)
+    } else {
+        full_path <- file.path(localdir, nomes)
+        colinas   <- lapply(full_path, function(fp) readRDS(fp))
+        return(colinas)
+    }
+}
