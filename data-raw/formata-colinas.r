10f7b4ba4ef58a3d7fd84ab989e2e457388009bd
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2023-12-28 17:20:41 -0300

    Modifica fonte de dados de colina

--- data-raw/formata-colinas.r
+++ data-raw/formata-colinas.r
@@ -2,20 +2,24 @@ library(data.table)
 library(curvacolina)
 
 ROOT <- "/mnt/c/Users/lucas/OneDrive/Área de Trabalho/colinas/tabelas"
-
+ROOT <- "/mnt/gtdp/Ciclo 3 - 2015 a 2024/_Resultados Finais por Usina/"
 dirs <- list.dirs(ROOT, recursive = FALSE)
 
 usi_ug <- list()
 
-for (dir_tab in dirs) {
-    dir_mod <- sub("tabelas", "modelos", dir_tab)
+for (dir in dirs) {
+    print(sub(".*Usina/+", "", dir))
 
-    tab <- fread(file.path(dir_tab, "colina_original", "associacao_maquinas_colina.csv"))
+    dir <- file.path(dir, "Curva de colina")
+    dir_mod <- file.path(dir, "modelos")
+
+    tab <- fread(file.path(dir, "original", "associacao_maquinas_colina.csv"),
+        colClasses = c("integer", "integer", "numeric", "numeric", "Date", "Date", "integer"))
     tab <- tab[!duplicated(ug, fromLast = TRUE)]
     usi_ug <- c(usi_ug, list(tab))
 
     arq_mods <- list.files(dir_mod, pattern = "_vaz_[[:digit:]]+.rds")
- 
+
     tag <- tab[, paste0("colina_", cod[1])]
     outarqs <- sub(".*vaz", tag, arq_mods)
     file.copy(file.path(dir_mod, arq_mods), file.path("data", outarqs))
